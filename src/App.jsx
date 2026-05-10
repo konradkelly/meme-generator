1e0282445092d077c79bfda9608744dbabf0e88c
konradkelly <konradky@gmail.com>
2026-05-10 13:59:57 -0700

    Add components, base styles, and images

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Header'
+import MainSection from './MainSection'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <MainSection />
+    </>
+  )
+}
+
+export default App
